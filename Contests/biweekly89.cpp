96bcb7294cc1d663529d0133284da8764f72439e
durgeshkk <durgeshkhandelwal2019@gmail.com>
2024-03-05 14:13:13 +0530

    Test: Develop Commits

--- Contests/biweekly89.cpp
+++ Contests/biweekly89.cpp
@@ -29,11 +29,6 @@ void google(int t) {cout << "Case #" << t << ": ";}
 ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0)n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
 */
  
-ll LCM(ll a,ll b){
-   ll ans = (a*b)/__gcd(a,b);
-   return ans;
-}
- 
 bool isPrime(int n){
    if (n <= 1)
        return false;
